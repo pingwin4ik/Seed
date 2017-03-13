d9cde762f4c4c4d9ccd8cd2d4867a258a9300c22
pc-hard <pc-hard@mail.ru>
2017-03-13 19:22:27 +0200

    Update seeds.groovy

--- seeds.groovy
+++ seeds.groovy
@@ -1,10 +1,13 @@
    pipelineJob('example') {  
-   scm {    git ('https://github.com/pc-hard/Seed.git')  }    
-   triggers {     scm('H/2 * * * *')  }    
+   scm {    
+      git ('https://github.com/pc-hard/Seed.git')  
+   }    
+   triggers {     scm('H/2 * * * *')  
+   }    
    definition {        
-     cps {            script(readFileFromWorkspace('dsl')
-     )            
+     cps {            
+        script(readFileFromWorkspace('dsl'))            
           sandbox()        
          }    
    }
- } (edited)
+ }
